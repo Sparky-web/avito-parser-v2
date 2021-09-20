fbc362fbb61ad20f11d21779b76bdbe3c475d3e0
Vlad BABINOV <vladbabinov@vlad.local>
2021-09-20 18:45:14 +0500

    s

--- modules/avito.js
+++ modules/avito.js
@@ -89,7 +89,7 @@ const getItemsFromSearch = async (link) => {
     }
 
     allItems = allItems.filter(el => {
-        console.log(link)
+        console.log(el)
         let a = el.title.match(new RegExp(link.query, "ig"))
         let b = !el.title.match(new RegExp(link.titleExclude, "ig"))
         let c = !el.description.match(new RegExp(link.descriptionExclude, "ig"))
